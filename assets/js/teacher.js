c6b00bf5a02d95256c6ae1ae07c296ab5adcc2bc
堀内麻子 <>
2019-05-21 13:03:52 +0900

    追加しました

--- assets/js/teacher.js
+++ assets/js/teacher.js
@@ -1,3 +1,50 @@
 $(function(){
-    $(".tagline").letterfx({"fx":"fly-right fly-top spin","timing":100});
-})
+
+    $(".tagline1").letterfx({"fx":"fly-right fly-top spin","timing":100});
+
+    $(window).scroll(function() {
+        //ドキュメントがスクロールされた量を取得する変数を作る
+        let scrollTop = $(document).scrollTop();
+        if( scrollTop > 80 ){
+            $('header').fadeOut(300);
+        }else{
+            $('header').fadeIn(300);
+        }
+         
+        // 2までスクロールされたら
+        let scrollTop2 = $('.programming').offset().top;
+
+        if (scrollTop2 > 100) {
+
+            $(".tagline2").letterfx({"fx":"fall","timing":150,"fx_duration":"950ms",});
+
+        } else  {
+
+        }
+        let scrollTop3 = $('.english').offset().top;
+
+        if(scrollTop3 > 100){
+
+            $(".tagline3").letterfx({"fx":"fly-right","timing":150,"fx_duration":"950ms",});
+
+        }
+        
+    });
+
+    let swiper = new Swiper('.swiper-container', {
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: true
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+        },
+      });
+    
+});
